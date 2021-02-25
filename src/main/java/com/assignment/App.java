279167623a534e3addb7f85e6038f6e0acf1d460
Alberto Delso Encabo <delsoencabo@hotmail.com>
2021-02-25 05:31:42 +0100

    Implementada la cache para el servicio de heroes

--- src/main/java/com/assignment/App.java
+++ src/main/java/com/assignment/App.java
@@ -3,10 +3,12 @@ package com.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
--- src/main/java/com/assignment/services/HeroesService.java
+++ src/main/java/com/assignment/services/HeroesService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import com.assignment.entity.Heroe;
 import com.assignment.repository.HeroesRepository;
@@ -20,6 +23,7 @@ public class HeroesService {
 		this.heroesRepository = heroesRepository;
 	}
 	
+	@Cacheable(value = "heroes", key = "#id")
 	public Optional<Heroe> getHeroeById(Long id) {		
 		Optional<Heroe> out = heroesRepository.findById(id);		
 		return out;
@@ -29,10 +33,12 @@ public class HeroesService {
 		return heroesRepository.findByLastNameLike(subString);
 	}
 	
+	@CacheEvict(value = "heroes", key = "#id")
 	public void delete(Long id) {
 		heroesRepository.deleteById(id);
 	}
 	
+	@CachePut(value = "heroes", key = "#heroe.id")
 	public Heroe save(Heroe heroe) {
 		return heroesRepository.save(heroe);
 	}
